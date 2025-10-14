cfec5f73e1fd5cf981e20dcce4f0fb232ba00124
Arthur Berlofa Bosi <berlofabosi.arthur@gmail.com>
2025-10-14 20:23:20 -0300

    Ex4 done

--- src/components/Ex4.jsx
+++ src/components/Ex4.jsx
@@ -4,9 +4,13 @@
 
 export default function Ex4() {
   return (
-    <div className="my-8">
+    <div className="my-8 p-0.5">
         <h1 className="text-2xl font-bold mb-4">Ex4 - Card Responsivo</h1>
-      {/* Resolva aqui */}
+        <div className="w-full md:w-sm bg-amber-300 p-3 rounded-2xl flex justify-self-center flex-col ">
+          <h1><b>Título</b></h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, provident? Asperiores odio aut id iusto laboriosam possimus quasi. Necessitatibus, magnam dolores iure similique in ipsum quis. Delectus voluptates eligendi ex.</p>
+          <button className="bg-blue-500 w-min p-2 rounded-2xl self-center hover:bg-blue-700 text-white text-justify">Botão</button>
+        </div>
     </div>
   );
 }
