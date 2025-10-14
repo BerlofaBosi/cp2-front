fa57b06ec9d8399cbe85edf57b7c135ff1b110c0
Arthur Berlofa Bosi <berlofabosi.arthur@gmail.com>
2025-10-14 20:54:59 -0300

    Ex6 done

--- src/components/Ex6.jsx
+++ src/components/Ex6.jsx
@@ -12,7 +12,11 @@ export default function Ex6() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex6 - Layout com Grid</h1>
-      {/* Resolva aqui */}
+        <div className="grid grid-rows-[80px_auto_80px] h-[20rem] gap-0" >
+          <div className="bg-emerald-400" />
+          <div className="bg-emerald-200" />
+          <div className="bg-emerald-400" />
+        </div>
     </div>
   );
 }
